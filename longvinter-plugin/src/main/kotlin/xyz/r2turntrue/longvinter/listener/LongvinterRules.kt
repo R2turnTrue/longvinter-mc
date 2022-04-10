59bd3e6c01f63ca64f617243773b375fdde17aff
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2022-04-10 22:10:42 +0900

    Hide join/quit message & default adventure mode

--- longvinter-plugin/src/main/kotlin/xyz/r2turntrue/longvinter/listener/LongvinterRules.kt
+++ longvinter-plugin/src/main/kotlin/xyz/r2turntrue/longvinter/listener/LongvinterRules.kt
@@ -1,14 +1,41 @@
 package xyz.r2turntrue.longvinter.listener
 
+import org.bukkit.GameMode
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityToggleSwimEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 
 class LongvinterRules: Listener {
 
     @EventHandler
     fun noSwim(event: EntityToggleSwimEvent) {
         event.isCancelled = true
+    }
+
+    @EventHandler
+    fun join(event: PlayerJoinEvent) {
+        event.run {
+            joinMessage(null)
+            player.gameMode = GameMode.ADVENTURE
+        }
+    }
+
+    @EventHandler
+    fun damage(event: EntityDamageEvent) {
+        if(event.entity is Player) {
+            if(event.cause == EntityDamageEvent.DamageCause.FALL) {
+                event.isCancelled = true
+            }
+        }
+    }
+
+    @EventHandler
+    fun quit(event: PlayerQuitEvent) {
+        event.quitMessage(null)
     }
 
 }
